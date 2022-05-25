5ab66f3add8364948d1b873e6c05c4cebce24b2e
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2022-05-25 17:00:20 -0300

    pixel_position

---
+++ pixel_position/main.js
@@ -0,0 +1,167 @@
+var image = document.getElementById("image");
+var canvas = document.getElementById('image-canvas');
+var context;
+
+let load = function (){
+    
+    context = canvas.getContext('2d');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context.drawImage(image, 0, 0);
+}
+
+let drawImage = function(cv, ctx, img) {
+    cv.width = img.width;
+    cv.height = img.height;
+    ctx.drawImage(img, 0, 0);
+}
+
+let greyScale = function() {
+    let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    let img = new MatrixImage(imageData);
+    for (var i = 0; i < img.width; i++) {
+        for (var j = 0; j < img.height; j++) {
+            var pixel = img.getPixel(i,j);
+            var gray = (pixel.red + pixel.green + pixel.blue) / 3; 
+            img.setPixel(i, j, new RGBColor(gray, gray, gray));
+        }
+    }
+    context.putImageData(img.imageData, 0, 0);
+}
+
+let mean = function() {
+    let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    let img = new MatrixImage(imageData);
+    for (var i = 0; i < img.width; i++) {
+        for (var j = 0; j < img.height; j++) {
+            var pixel = Array();
+            pixel.push(img.getPixel(i-1,j-1).red);
+            pixel.push(img.getPixel(i-1,j).red);
+            pixel.push(img.getPixel(i,j-1).red);
+            pixel.push(img.getPixel(i+1,j-1).red);
+            pixel.push(img.getPixel(i,j).red);
+            pixel.push(img.getPixel(i-1,j+1).red);
+            pixel.push(img.getPixel(i,j+1).red);
+            pixel.push(img.getPixel(i+1,j).red);
+            pixel.push(img.getPixel(i+1,j+1).red);
+            var gray = pixel.reduce((a, b) => a + b, 0) / 9;
+    
+            img.setPixel(i, j, new RGBColor(gray, gray, gray));
+        }
+    }
+    context.putImageData(img.imageData, 0, 0);
+}
+
+let red = function(){
+    let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    let img = new MatrixImage(imageData);
+
+    for (var i = 0; i < img.width; i++) {
+        for (var j = 0; j < img.height; j++) {
+            var pixel = Array();
+            pixel.push(img.getPixel(i-1,j-1).red);
+            pixel.push(img.getPixel(i,j-1).red);
+            pixel.push(img.getPixel(i+1,j-1).red);
+            pixel.push(img.getPixel(i-1,j).red);
+            pixel.push(img.getPixel(i,j).red);
+            pixel.push(img.getPixel(i+1,j).red);
+            pixel.push(img.getPixel(i-1,j+1).red);
+            pixel.push(img.getPixel(i,j+1).red);
+            pixel.push(img.getPixel(i+1,j+1).red);
+            var gray = img.getPixel(i,j).red;
+    
+            img.setPixel(i, j, new RGBColor(gray, gray, gray));
+        }
+    }
+    context.putImageData(img.imageData, 0, 0);
+}
+
+let green = function(){
+    let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    let img = new MatrixImage(imageData);
+
+    for (var i = 0; i < img.width; i++) {
+        for (var j = 0; j < img.height; j++) {
+            var pixel = Array();
+            pixel.push(img.getPixel(i-1,j-1).red);
+            pixel.push(img.getPixel(i,j-1).red);
+            pixel.push(img.getPixel(i+1,j-1).red);
+            pixel.push(img.getPixel(i-1,j).red);
+            pixel.push(img.getPixel(i,j).red);
+            pixel.push(img.getPixel(i+1,j).red);
+            pixel.push(img.getPixel(i-1,j+1).red);
+            pixel.push(img.getPixel(i,j+1).red);
+            pixel.push(img.getPixel(i+1,j+1).red);
+            var gray = img.getPixel(i,j).green;
+    
+            img.setPixel(i, j, new RGBColor(gray, gray, gray));
+        }
+    }
+    context.putImageData(img.imageData, 0, 0);
+}
+
+let blue = function(){
+    let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    let img = new MatrixImage(imageData);
+
+    for (var i = 0; i < img.width; i++) {
+        for (var j = 0; j < img.height; j++) {
+            var pixel = Array();
+            pixel.push(img.getPixel(i-1,j-1).red);
+            pixel.push(img.getPixel(i,j-1).red);
+            pixel.push(img.getPixel(i+1,j-1).red);
+            pixel.push(img.getPixel(i-1,j).red);
+            pixel.push(img.getPixel(i,j).red);
+            pixel.push(img.getPixel(i+1,j).red);
+            pixel.push(img.getPixel(i-1,j+1).red);
+            pixel.push(img.getPixel(i,j+1).red);
+            pixel.push(img.getPixel(i+1,j+1).red);
+            var gray = img.getPixel(i,j).blue;
+    
+            img.setPixel(i, j, new RGBColor(gray, gray, gray));
+        }
+    }
+    context.putImageData(img.imageData, 0, 0);
+}
+
+class RGBColor {
+    constructor(r, g, b) {
+      this.red = r;
+      this.green = g; 
+      this.blue = b;
+    }
+}
+
+class MatrixImage {
+    constructor(imageData) {
+      this.imageData = imageData;
+      this.height = imageData.height; 
+      this.width = imageData.width;
+    }
+
+    getPixel(x, y) {
+        let position = ((y * (this.width * 4)) + (x * 4));
+
+        return new RGBColor(
+             this.imageData.data[position],   //red
+             this.imageData.data[position+1], //green
+             this.imageData.data[position+2], //blue
+        );
+    }
+
+    setPixel(x, y, color) {
+        let position = ((y * (this.width * 4)) + (x * 4));
+        this.imageData.data[position] = color.red;
+        this.imageData.data[position+1] = color.green;
+        this.imageData.data[position+2] = color.blue;
+    }
+}
+
+document.getElementById('btnLoad').addEventListener('click', load);
+document.getElementById('btnGreyScale').addEventListener('click', greyScale);
+document.getElementById('btnMean').addEventListener('click', mean);
+document.getElementById('btnRed').addEventListener('click', red);
+document.getElementById('btnGreen').addEventListener('click', green);
+document.getElementById('btnGreen').addEventListener('click', green);
+document.getElementById('btnBlue').addEventListener('click', blue);
+
