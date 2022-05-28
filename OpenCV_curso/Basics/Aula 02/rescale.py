c95d0c603aa5b1104609de0128010074b749ea1e
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2022-05-28 19:59:50 -0300

    OpenCV Aula2

---
+++ OpenCV_curso/Basics/Aula 02/rescale.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+
+# img = cv.imread('../Photos/cat_large.jpg')
+
+# cv.imshow('Cat', img)
+
+
+def rescaleFrame(frame, scale=0.75):
+    # Images, Videos and Live Videos
+    
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+def changeRes(width, height):
+    #Live Video
+
+    capture.set(3, width)
+    capture.set(4, height)
+
+
+img_resized = rescaleFrame(img)
+cv.imshow('Cat resized', img_resized)
+
+cv.waitKey(0)
+
+capture = cv.VideoCapture('../Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+
+    frame_resized = rescaleFrame(frame)
+
+
+    cv.imshow('Video', frame)
+    cv.imshow('Video Resized', frame_resized)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
