f9abc5e2383ecb0afd6e70eac9e179185d2d8ac6
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2022-05-29 15:44:35 -0300

    Open CV Aula 04

---
+++ OpenCV_curso/Basics/Aula 04/basic.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+from cv2 import resize
+
+img = cv.imread('../Photos/park.jpg')
+cv.imshow('Park', img)
+
+# Converting to grayscale
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#cv.imshow('Gray', gray)
+
+# Blur
+
+blur = cv.GaussianBlur(gray, (3,3), cv.BORDER_DEFAULT)
+# cv.imshow('Blur', blur)
+
+# Edge Cascade
+
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny', canny)
+
+# Dilating the image
+
+dilated = cv.dilate(canny, (3,3), iterations=1 )
+cv.imshow('Dilated', dilated)
+
+# Eroding
+
+eroding = cv.erode(dilated, (3,3), iterations=1)
+cv.imshow('Eroding', eroding)
+
+# Resize
+
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+# Cropping
+
+cropped = img[50:200, 200:400]
+cv.imshow('Cropped', cropped)
+
+
+
+cv.waitKey(0)
