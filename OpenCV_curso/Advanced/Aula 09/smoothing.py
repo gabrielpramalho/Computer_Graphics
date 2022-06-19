a7c329bf4655c81391dc6e80adfe3ac4fb014157
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2022-06-18 21:35:45 -0300

    aulas openCV

--- OpenCV_curso/Advanced/Aula 09/smoothing.py
+++ OpenCV_curso/Advanced/Aula 09/smoothing.py
@@ -3,4 +3,24 @@ import cv2 as cv
 img = cv.imread('../Photos/cats.jpg')
 cv.imshow('Cats', img)
 
+
+# Average Blur
+average = cv.blur(img, (3,3))
+cv.imshow('Average', average)
+
+# Gaussian Blur
+
+gauss = cv.GaussianBlur(img, (3,3), 0)
+cv.imshow('Gaussian', gauss)
+
+# Median Blur
+
+meduian = cv.medianBlur(img, 3)
+cv.imshow('Median', meduian)
+
+# Bilateral Blur
+
+bilateral = cv.bilateralFilter(img, 5, 15, 15)
+cv.imshow('Bilateral', bilateral)
+
 cv.waitKey(0)
