3bf09bea2a8ac09b6a826d3690d63a1788ce7532
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2022-06-22 10:55:47 -0300

    ocr

--- OCR/main.py
+++ OCR/main.py
@@ -3,7 +3,18 @@ import pytesseract as tess
 
 tess.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-img = cv.imread('Images/test.jpg')
+img = cv.imread('Images/teste2.png')
+# cv.imshow('IMG', img)
+
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('IMG', gray)
+
+text = tess.image_to_string(img, lang='por')
+
+print(text)
+print(tess.image_to_boxes(img))
+
+
+cv.waitKey(0)
 
 
